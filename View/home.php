bc03a7e07cfc896720fe080a95f764b8f57b1127
Sieglinde <sieglindevwb@gmail.com>
2024-01-19 16:55:56 +0100

    Here is no content - in homepage.

--- View/home.php
+++ View/home.php
@@ -5,7 +5,7 @@
 <section style="background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
     <p style="color: #333;"><a href="index.php?page=articles-index" style="color: #ff69b4; text-decoration: none;">To articles page</a></p>
 
-    <p style="color: #666; font-size: 14px;">Put your content here.</p>
+    <p style="color: #666; font-size: 14px;">No content here</p>
 </section>
 
 <?php require 'View/includes/footer.php'?>
